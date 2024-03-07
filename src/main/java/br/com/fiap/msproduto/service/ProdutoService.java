820f67b9bbef553e264afd28673f8ab59281745f
bruno-gds <brunog.damascena@gmail.com>
2024-03-07 19:34:50 -0300

    finalizando CRUD na Service

--- src/main/java/br/com/fiap/msproduto/service/ProdutoService.java
+++ src/main/java/br/com/fiap/msproduto/service/ProdutoService.java
@@ -3,9 +3,12 @@ package br.com.fiap.msproduto.service;
 import br.com.fiap.msproduto.model.Produto;
 import br.com.fiap.msproduto.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * @author Bruno Gomes Damascena dos santos (bruno-gds) < brunog.damascena@gmail.com >
@@ -22,5 +25,44 @@ public class ProdutoService {
 
     public List<Produto> listarProduto() {
         return produtoRepository.findAll();
+    }
+
+    public Produto cadastrarProduto(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public ResponseEntity<?> listarUmProduto(Integer produtoId) {
+        Produto produto = produtoRepository.findById(produtoId).orElse(null);
+
+        if (produto != null) {
+            return ResponseEntity.ok(produto);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado.");
+        }
+    }
+
+    public Produto atualizarProduto(Integer produtoId, Produto novoProduto) {
+        Produto produtoExistente = produtoRepository.findById(produtoId).orElse(null);
+
+        if (produtoExistente != null) {
+            produtoExistente.setNome(novoProduto.getNome());
+            produtoExistente.setDescricao(novoProduto.getDescricao());
+            produtoExistente.setQuantidade_estoque(novoProduto.getQuantidade_estoque());
+            produtoExistente.setPreco(novoProduto.getPreco());
+
+            return produtoRepository.save(produtoExistente);
+        } else {
+            throw new NoSuchElementException("Produto não encontrado!");
+        }
+    }
+
+    public void excluirProduto(Integer produtoId) {
+        Produto produtoExistente = produtoRepository.findById(produtoId).orElse(null);
+
+        if (produtoExistente != null) {
+            produtoRepository.delete(produtoExistente);
+        } else {
+            throw new NoSuchElementException("Produto não encontrado!");
+        }
     }
 }
